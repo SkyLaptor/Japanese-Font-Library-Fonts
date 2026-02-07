451c1ec67637576345aa1c37794478b953c6d5d0
skylaptor <gbreakmasterfrom@gmail.com>
2026-02-08 06:37:51 +0900

    修正
    あまり意味がなかったサイズ計算モードを廃止
    横幅自動調整モード追加

--- scripts/convert_for_skyrim.py
+++ scripts/convert_for_skyrim.py
@@ -18,23 +18,25 @@ BASE_FONT_BOOK = "skyrim_jp_book_optimized.ttf"
 BASE_FONT_HAND = "skyrim_jp_hand_optimized.ttf"
 ASCENT = 880
 DESCENT = 144
-SHIFT_HEIGHT_EVERY = -72
-SHIFT_HEIGHT_BOOK = -72
-SHIFT_HEIGHT_HAND = -72
-RESIZE_MODE_V = "v"
-RESIZE_MODE_VH = "vh"
-RESIZE_MODE_HE = "he"
+MODE_WIDTH_AUTO = "auto"
+MODE_WIDTH_CONDENSED = "cond"
+MODE_WIDTH_SKINNY = "skin"
+TARGET_RATIO_CONDENSED = 0.64
+TARGET_RATIO_SKINNY = 0.42
+SHIFT_HEIGHT_EVERY = -96
+SHIFT_HEIGHT_BOOK = -96
+SHIFT_HEIGHT_HAND = -96
 
-def main(input_font_path, output_font_path="", subset_chars_path="", mode_ui="", ratio_width=100, resize_mode=RESIZE_MODE_V):
+def main(input_font_path, output_font_path="", subset_chars_path="", mode_ui=MODE_UI_EVERY, mode_width=MODE_WIDTH_AUTO, shift_height=None):
     """Convert the specified font for Skyrim's UI
 
            Args:
                input_font_path (str): Font file paths subject to converion.
                output_font_path (str, optional): Output font file path. The file extension must be ttf. Default: ''
                subset_chars_path (str, Optional): Subset character file path. Default: ''
-               mode_ui (str, optional): UI mode(every,book,hand). Default: ''
-               ratio_width (int, optional): Width specification(%). Default: 100
-               resize_mode (str, optional): Resize mode(v,vh,he). Default: 'v'
+               mode_ui (str, optional): UI mode(every,book,hand). Default: 'every'
+               mode_width (str, optional): Width mode(auto,cond,skin). Default: 'auto'
+               shift_height (int, optional): Height adjustment value(units). Thick for positive values, thin for negative values. Default: None
 
            Returns:
                str: Output font file path.
@@ -42,55 +44,51 @@ def main(input_font_path, output_font_path="", subset_chars_path="", mode_ui="",
     print("=== Start of Convert the specified font for Skyrim's UI ===")
 
     base_font_path = ""
-    shift_height = 0
     if mode_ui == MODE_UI_EVERY:
         print("Convert for Everywhere UI.")
         base_font_path = BASE_FONT_EVERY
-        shift_height = SHIFT_HEIGHT_EVERY
+        if shift_height is None:
+            shift_height = SHIFT_HEIGHT_EVERY
     elif mode_ui == MODE_UI_BOOK:
         print("Convert for Book UI.")
         base_font_path = BASE_FONT_BOOK
-        shift_height = SHIFT_HEIGHT_BOOK
+        if shift_height is None:
+            shift_height = SHIFT_HEIGHT_BOOK
     elif mode_ui == MODE_UI_HAND:
         print("Convert for Handwritten UI.")
         base_font_path = BASE_FONT_HAND
-        shift_height = SHIFT_HEIGHT_HAND
+        if shift_height is None:
+            shift_height = SHIFT_HEIGHT_HAND
     else:
         msg = f"The UI mode is incorrect. Please select from the following options. {MODE_UI_EVERY}, {MODE_UI_BOOK}, {MODE_UI_HAND}"
         logging.error(msg)
         raise ValueError(msg)
+    if mode_width != MODE_WIDTH_AUTO and mode_width != MODE_WIDTH_CONDENSED and mode_width != MODE_WIDTH_SKINNY:
+        msg = f"The Width mode is incorrect. Please select from the following options. {MODE_WIDTH_AUTO}, {MODE_WIDTH_CONDENSED}, {MODE_WIDTH_SKINNY}"
+        logging.error(msg)
+        raise ValueError(msg)
+
 
     print("Calculating the resize factor...")
     base_result = average_glyph_metrics.main(base_font_path)
     input_result = average_glyph_metrics.main(input_font_path)
-
-    ratio_total = 100
-    if resize_mode == RESIZE_MODE_V:
-        print(f"Vertical average value of the base font: {base_result[1]}units")
-        print(f"Vertical average value of the input font: {input_result[1]}units")
-        ratio_h = base_result[1] / input_result[1]
-        ratio_total = round(ratio_h * 100)
-    elif resize_mode == RESIZE_MODE_VH:
-        print(f"Vertical average value of the base font: {base_result[1]}units")
-        print(f"Vertical average value of the input font: {input_result[1]}units")
-        print(f"Horizontal average value of the base font: {base_result[0]}units")
-        print(f"Horizontal average value of the input font: {input_result[0]}units")
-        ratio_h = base_result[1] / input_result[1]
-        ratio_w = base_result[0] / input_result[0]
-        ratio_total = round(((ratio_h + ratio_w) / 2) * 100)
-    elif resize_mode == RESIZE_MODE_HE:
-        print(f"Vertical average value of the base font: {base_result[1]}units")
-        print(f"Vertical average value of the input font: {input_result[1]}units")
-        ratio_h = base_result[1] / input_result[1]
-        ratio_total = round(ratio_h * 100)
-        ratio_width = round((base_result[0] / (input_result[0] * ratio_total)) * 100) + ratio_width
-        print(f"Width factor is: {ratio_width}%")
+    print(f"Vertical average value of the base font: {base_result[1]}units")
+    print(f"Horizontal average value of the base font: {base_result[0]}units")
+    print(f"Vertical average value of the input font: {input_result[1]}units")
+    print(f"Horizontal average value of the input font: {input_result[0]}units")
+    ratio_h = base_result[1] / input_result[1]
+    ratio_w = input_result[0] * ratio_h
+    ratio_total = round(ratio_h * 100)
+    if mode_width == MODE_WIDTH_CONDENSED:
+        ratio_width = round((base_result[0] * TARGET_RATIO_CONDENSED / ratio_w) * 100)
+    elif mode_width == MODE_WIDTH_SKINNY:
+        ratio_width = round((base_result[0] * TARGET_RATIO_SKINNY / ratio_w) * 100)
     else:
-        msg = f"The Resize mode is incorrect. Please select from the following options. {RESIZE_MODE_V}, {RESIZE_MODE_VH}, {RESIZE_MODE_HE}"
-        logging.error(msg)
-        raise ValueError(msg)
-
+        ratio_width = round((base_result[0] / ratio_w) * 100)
+    if ratio_width > 100:
+        ratio_width = 100
     print(f"Resize factor is: {ratio_total}%")
+    print(f"Width factor is: {ratio_width}% (WidthMode: {mode_width})")
 
     print("Converting fonts...")
     output_font_path = optimize_font.main(input_font_path=input_font_path, output_font_path=output_font_path, subset_chars_path=subset_chars_path, ascent=ASCENT, descent=DESCENT, ratio_total=ratio_total, ratio_width=ratio_width, shift_height=shift_height)
@@ -102,12 +100,12 @@ def main(input_font_path, output_font_path="", subset_chars_path="", mode_ui="",
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert the specified font for Skyrim's UI")
     
-    parser.add_argument("-i", "--input", required=True, help="Font file paths subject to converion.")
-    parser.add_argument("-o", "--output", default="", help="Output font file path. The file extension must be ttf.")
-    parser.add_argument("--subset", default="", help="Subset character file path.")
-    parser.add_argument("--mode_ui", default="", help="UI mode(every,book,hand).")
-    parser.add_argument("--ratio_width", type=int, default=100, help=f"Width specification(%%).")
-    parser.add_argument("--resize_mode", default="v", help="Resize mode(v,vh,he).")
+    parser.add_argument("-i", "--input", type=str, required=True, help="Font file paths subject to converion.")
+    parser.add_argument("-o", "--output", type=str, default="", help="Output font file path. The file extension must be ttf.")
+    parser.add_argument("-s", "--subset", type=str, default="", help="Subset character file path.")
+    parser.add_argument("-m", "--mode_ui", type=str, default=MODE_UI_EVERY, help="UI mode(every,book,hand).")
+    parser.add_argument("-w", "--mode_width", type=str, default=MODE_WIDTH_AUTO, help=f"Width mode(auto,cond,skin).")
+    parser.add_argument("--shift_height", type=int, default=None, help=f"Height adjustment value(units). Thick for positive values, thin for negative values.")
     
     if len(sys.argv) == 1:
         parser.print_help()
@@ -120,6 +118,6 @@ if __name__ == "__main__":
         output_font_path=args.output,
         subset_chars_path=args.subset,
         mode_ui=args.mode_ui,
-        ratio_width=args.ratio_width,
-        resize_mode=args.resize_mode
+        mode_width=args.mode_width,
+        shift_height=args.shift_height
     )
